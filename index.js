4ec223357cd04bd60d25d550890ff2715edc31e0
Carla <ruixdsgn@gmail.com>
2023-05-11 20:05:26 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  // create a function that takes a string as an argument
+  // evaluate that argument in the block of code to check if the argument is a palindrome
+  // if the word is a plindrome
+  // return true, otherise false
 }
 
 /* 
-  Add your pseudocode here
+  a function that thats a string as an input
+
+    if the input evaluates as a palindrome
+      return true
+    else
+      return false
+  
+  use the function to accept a string as an argument to test the code
 */
 
+function isPalindrome(string) {
+  if (string === string.split('').reverse().join('')) {
+    return true
+  }
+  else {
+    return false
+  }
+  
+}
+
+isPalindrome("racecar")
+
 /*
-  Add written explanation of your solution here
+  I created a function that takes in a string as an argument
+  I used an if else statement to evaluate whether a string is palindrome
+  If a string is a palindrome, the return should state true, otherwise false
 */
 
 // You can run `node index.js` to view these console logs
